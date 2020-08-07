5cd03d8c401313008dedac562d38d711427fb1bb
sdudnyk <sergey101dudni@gmail.com>
2020-08-07 13:58:39 +0300

    initial

---
+++ build/precache-manifest.8ccd0b6df76b7599746dc0090e80d842.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41b4c458791f7f0ccd2a45704fedeaf0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "672873a0334fb227b6cb",
+    "url": "/static/css/main.2e4e4369.chunk.css"
+  },
+  {
+    "revision": "81a037f6fa61a1d13b44",
+    "url": "/static/js/2.29af4f0e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.29af4f0e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "672873a0334fb227b6cb",
+    "url": "/static/js/main.fb635fc1.chunk.js"
+  },
+  {
+    "revision": "66f538133d580fcaed64",
+    "url": "/static/js/runtime-main.fd715b68.js"
+  }
+]);
